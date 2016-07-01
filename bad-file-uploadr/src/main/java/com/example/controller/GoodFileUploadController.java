a745d73bf4bbb9eaf1255d67b87ce5d32f94886e
Daniel Aquino <daniel.j.aquino@gmail.com>
2016-06-30 19:30:17 -0500

    Moving around bad and good file upload implementations.

--- bad-file-uploadr/src/main/java/com/example/controller/GoodFileUploadController.java
+++ bad-file-uploadr/src/main/java/com/example/controller/GoodFileUploadController.java
@@ -3,7 +3,7 @@ package com.example.controller;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
+import java.nio.file.StandardCopyOption;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,29 +12,29 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.multipart.MultipartFile;
 
 @Controller
-@RequestMapping("/bad-file-upload")
-public class BadFileUploadController {
+@RequestMapping("/good-file-upload")
+public class GoodFileUploadController {
     private final String ROOT = "uploads";
 
     @RequestMapping(method = RequestMethod.GET)
     public String renderBadFileUploadPage() {
-        return "bad/file-upload";
+        return "good/file-upload";
     }
 
     @RequestMapping(method = RequestMethod.POST)
-    public String handleBadFileUpload(@RequestParam("file") MultipartFile file) {
+    public String handleGoodFileUpload(@RequestParam("file") MultipartFile file) {
         if (!file.isEmpty()) {
             try {
                 if(!Files.isDirectory(Paths.get(ROOT))) {
                     Files.createDirectory(Paths.get(ROOT));
                 }
-                Files.write(Paths.get(ROOT, file.getOriginalFilename()), file.getBytes(), StandardOpenOption.CREATE);
+                Files.copy(file.getInputStream(), Paths.get(ROOT, file.getOriginalFilename()), StandardCopyOption.REPLACE_EXISTING);
             }
             catch (IOException|RuntimeException exc) {
                 exc.printStackTrace();
-                return "bad/file-upload-failed";
+                return "good/file-upload-failed";
             }
         }
-        return "bad/file-upload-success";
+        return "good/file-upload-success";
     }
 }
